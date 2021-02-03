44a80a68c5dac206ba525ea584aebe93e6f22533
Kushalkumar23 <kushalmittal2203@gmail.com>
2021-02-03 09:32:56 +0530

    Add files via upload

---
+++ sql/sqlscript.sql
@@ -0,0 +1,131 @@
+REM   Script: Session 02
+REM   Activity 5
+
+select * from salseman;
+
+select * from salesman;
+
+CREATE TABLE salesman;
+
+CREATE TABLE salesman;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+describe salseman
+
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+describe salesman
+
+
+INSERT all  
+INTO salesman VALUES ( 5001, 'James Hoog', 'New York', 15) 
+INTO salesman VALUES(5002,'Nail Knite', 'Paris', 13) 
+INTO salesman VALUES (5005, 'Pit Alex', 'London', 11 
+INTO salesman VALUES (5006, 'McLyon', 'Paris', 14) 
+INTO salesman VALUES (5007, 'Paul Adam', 'Rome', 13)  
+INTO salesman VALUES (5003, 'Lauson Hen', 'San Jose', 12);
+
+INSERT all  
+INTO salesman VALUES (5001, 'James Hoog', 'New York', 15) 
+INTO salesman VALUES (5002,'Nail Knite', 'Paris', 13) 
+INTO salesman VALUES (5005, 'Pit Alex', 'London', 11 
+INTO salesman VALUES (5006, 'McLyon', 'Paris', 14) 
+INTO salesman VALUES (5007, 'Paul Adam', 'Rome', 13)  
+INTO salesman VALUES (5003, 'Lauson Hen', 'San Jose', 12);
+
+INSERT all  
+INTO salesman VALUES (5001, 'James Hoog', 'New York', 15) 
+INTO salesman VALUES (5002,'Nail Knite', 'Paris', 13) 
+INTO salesman VALUES (5005, 'Pit Alex', 'London', 11 
+INTO salesman VALUES (5006, 'McLyon', 'Paris', 14) 
+INTO salesman VALUES (5007, 'Paul Adam', 'Rome', 13)  
+INTO salesman VALUES (5003, 'Lauson Hen', 'San Jose', 12);
+
+INSERT all  
+INTO salesman VALUES (5001, 'James Hoog', 'New York', 15), 
+INTO salesman VALUES (5002,'Nail Knite', 'Paris', 13);
+
+INSERT all  
+INTO salesman VALUES (5001, 'James Hoog', 'New York', 15) 
+INTO salesman VALUES (5002,'Nail Knite', 'Paris', 13) 
+INTO salesman VALUES (5005, 'Pit Alex', 'London', 11) 
+INTO salesman VALUES (5006, 'McLyon', 'Paris', 14) 
+INTO salesman VALUES (5007, 'Paul Adam', 'Rome', 13)  
+INTO salesman VALUES (5003, 'Lauson Hen', 'San Jose', 12);
+
+INSERT all  
+INTO salesman VALUES (5001, 'James Hoog', 'New York', 15) 
+INTO salesman VALUES (5002,'Nail Knite', 'Paris', 13) 
+INTO salesman VALUES (5005, 'Pit Alex', 'London', 11) 
+INTO salesman VALUES (5006, 'McLyon', 'Paris', 14) 
+INTO salesman VALUES (5007, 'Paul Adam', 'Rome', 13)  
+INTO salesman VALUES (5003, 'Lauson Hen', 'San Jose', 12);
+
+select 1 from dual;
+
+INSERT all  
+INTO salesman VALUES (5001, 'James Hoog', 'New York', 15) 
+INTO salesman VALUES (5002,'Nail Knite', 'Paris', 13) 
+INTO salesman VALUES (5005, 'Pit Alex', 'London', 11) 
+INTO salesman VALUES (5006, 'McLyon', 'Paris', 14) 
+INTO salesman VALUES (5007, 'Paul Adam', 'Rome', 13)  
+INTO salesman VALUES (5003, 'Lauson Hen', 'San Jose', 12) 
+select 1 from dual;
+
+SELECT salesman_id, salesman_city FROM salesman;
+
+SELECT * FROM salesman WHERE salesman_city='Paris';
+
+SELECT salesman_id, commission FROM salesman WHERE salesman_name='Paul Adam';
+
+ALTER TABLE salesman ADD grade int;
+
+describe salesman
+
+
+UPDATE salesman SET grade=100;
+
+select * from salseman;
+
+select * from salesman;
+
+salect * from salesman  
+
+
+select * from salesman  ;
+
+select * from salesman   
+ 
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+
+UPDATE salesman SET grade=200 WHERE salesman_city='Rome';
+
+select * from salesman  ;
+
+UPDATE salesman SET grade=300 WHERE salesman_name='James Hoog';
+
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+
+select * from salesman  ;
+
